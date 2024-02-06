2e629d667d079e02d0085287f7fdede9b9c58687
Paul Thompson <paul@findingfocus.dev>
2024-02-05 23:04:33 -0700

    Adding Notes file

---
+++ Notes.lua
@@ -0,0 +1,17 @@
+X_Add Gameboy resolution screen
+X_Add Kvothe Sprite
+X_Moving Kvothe with input
+X_Clamping movement for Kvothe
+X_Add HUD Stand-in
+
+Todo
+__Draw a whole screen using tile keycodes
+__Translate all images off screen when Kvothe walks offscreen
+
+
+Next Episode
+__Add Repeating ground tile
+__Add all colors to color constants palette
+__New Sonic Pi Music
+
+
