456ca274a6d671927d753bcbd49bda657a9c1f69
Paul Thompson <paul@findingfocus.dev>
2024-07-01 21:12:42 -0600

    added water anims, but animation only working upon scene refresh

--- src/InsertAnimation.lua
+++ src/InsertAnimation.lua
@@ -4,6 +4,7 @@ function InsertAnimation:init(mapRow, mapColumn)
     self.mapRow = mapRow
     self.mapColumn = mapColumn
     self.flowers = AnimSpitter(1012, 1015, 0.75)
+    self.water = AnimSpitter(102, 105, .25)
 
     table.insert(MAP[1][1].animatables, function() insertAnim(2, 3, self.flowers.frame) end)
     table.insert(MAP[1][1].animatables, function() insertAnim(5, 7, self.flowers.frame) end)
@@ -13,10 +14,15 @@ function InsertAnimation:init(mapRow, mapColumn)
     table.insert(MAP[1][2].animatables, function() insertAnim(4, 9, self.flowers.frame) end)
 
     table.insert(MAP[2][1].animatables, function() insertAnim(2, 2, self.flowers.frame) end)
+    table.insert(MAP[7][2].animatables, function() insertAnim(4, 1, self.water.frame) end)
+    table.insert(MAP[7][2].animatables, function() insertAnim(4, 2, self.water.frame) end)
+    table.insert(MAP[7][2].animatables, function() insertAnim(5, 1, self.water.frame) end)
+    table.insert(MAP[7][2].animatables, function() insertAnim(5, 2, self.water.frame) end)
 end
 
 function InsertAnimation:update(dt)
     self.flowers:update(dt)
+    self.water:update(dt)
 
     for i = 1, #MAP[self.mapRow][self.mapColumn].animatables do
         MAP[self.mapRow][self.mapColumn].animatables[i]()
