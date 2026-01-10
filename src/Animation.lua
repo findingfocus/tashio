c412935b4f01b47c082a6f50d066dc076c9d768c
Paul Thompson <paul@findingfocus.dev>
2026-01-09 20:27:52 -0700

    animation logic bugs

--- src/Animation.lua
+++ src/Animation.lua
@@ -29,7 +29,8 @@ function Animation:update(dt)
     if self.timer > self.interval then
       self.timer = self.timer % self.interval
 
-      self.currentFrame = math.max(1, (self.currentFrame + 1) % (#self.frames + 1))
+      --self.currentFrame = math.max(1, (self.currentFrame + 1) % (#self.frames + 1))
+      self.currentFrame = ((self.currentFrame) % #self.frames) + 1
 
       if self.currentFrame == #self.frames then
         self.timesPlayed = self.timesPlayed + 1
